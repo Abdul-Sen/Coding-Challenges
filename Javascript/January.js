1596ca2ace8ef43c08572187f9a0f3a04c3679ca
Abdul Rehman <abdul-rehman@myseneca.ca>
2019-01-24 22:34:28 -0600

    Added scrable function that needs perforamce improvement

--- Javascript/January.js
+++ Javascript/January.js
@@ -220,3 +220,41 @@ function alphabetPosition(text) {
     return charPos;
   }
 
+/*
+Complete the function scramble(str1, str2) that returns true if a portion of str1 characters
+can be rearranged to match str2, otherwise returns false.
+
+Notes:
+Only lower case letters will be used (a-z). No punctuation or digits will be included.
+Performance needs to be considered
+Input strings s1 and s2 are null terminated.
+*/
+//TODO: Currently this is TOO SLOW! Find a faster alternative
+function scramble(str1, str2) {
+      let str1Temp  = str1.split("");
+      let str2Temp = str2.split("");
+      if(str1.length < str2.length)
+      {
+          return false;
+      }
+      let finalString = "";
+      for(let i = 0 ; i < str2Temp.length; i++)
+      {
+          for(let j = 0 ; j < str1Temp.length; j++)
+          {
+              if(str2Temp[i] == str1Temp[j])
+              {
+                 finalString += str1Temp.splice(j, 1);
+                 console.log(str1Temp);
+                 break;
+              }
+          }
+      }
+      if(finalString == str2)
+      {
+          return true;
+      }
+      return false;
+}
+
+console.log(scramble("aabbcamaomsccdd","commas"));
